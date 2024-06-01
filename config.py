20e49c6c67b8c3395cd1c74233d30d582958691a
stepanrt-ss <sjcam228@gmail.com>
2024-06-01 03:01:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,13 @@
+import pymysql
+
 host = "93.93.207.69"
 user = "gen_user"
 password = "SvF49R?Jivn@(b"
 db_name = "default_db"
+con = pymysql.connect(
+                host=host,
+                port=3306,
+                user=user,
+                password=password,
+                database=db_name,
+                cursorclass=pymysql.cursors.DictCursor)
